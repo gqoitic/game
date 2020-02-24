c027d28c724d4f80ad2c691543617b213e840d22
gqoitic <gqoitic@gmail.com>
2020-02-24 18:19:42 +0500

    changed healthbar colors

--- src/main/java/com/gqoitic/game/HUD.java
+++ src/main/java/com/gqoitic/game/HUD.java
@@ -23,7 +23,8 @@ public class HUD {
     public void render(Graphics g) {
         g.setColor(Color.gray);
         g.fillRect(15, 15, 200, 32);
-        g.setColor(new Color(75, greenValue, 0));
+//        g.setColor(new Color(75, greenValue, 0));
+        g.setColor(Color.getHSBColor((1f * HEALTH) / 360, 1f, 1f));
         g.fillRect(15, 15, HEALTH * 2, 32);
         g.setColor(Color.white);
         g.drawRect(15, 15, 200, 32);
